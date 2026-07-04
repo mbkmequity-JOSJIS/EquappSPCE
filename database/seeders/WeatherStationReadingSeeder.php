33739594f80b29a6a2c5da9937bf661aa46c28e5
DESKTOP-CAIGIPS\Asus <hendraharyono0@gmail.com>
2026-07-04 15:31:52 +0700

    fix faker code for seeder

--- database/seeders/WeatherStationReadingSeeder.php
+++ database/seeders/WeatherStationReadingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class WeatherStationReadingSeeder extends Seeder
 {
@@ -18,14 +19,14 @@ class WeatherStationReadingSeeder extends Seeder
                 $readings[] = [
                     // GUNAKAN device_code, bukan device_id
                     'device_code' => $device->device_code,
-                    'intensitas_hujan' => fake()->randomFloat(2, 0.00, 150.00),
-                    'kecepatan_angin' => fake()->randomFloat(2, 0.00, 40.00),
-                    'kelembaban' => fake()->randomFloat(2, 40.00, 99.00),
-                    'pm25' => fake()->randomFloat(2, 5.00, 150.00),
-                    'tekanan' => fake()->randomFloat(2, 1000.00, 1025.00),
-                    'temperature' => fake()->randomFloat(2, 22.00, 35.00),
-                    'total_hujan' => fake()->randomFloat(2, 0.00, 500.00),
-                    'uv_index' => fake()->randomFloat(2, 0.00, 11.00),
+                    'intensitas_hujan' => Faker::create()->randomFloat(2, 0.00, 150.00),
+                    'kecepatan_angin' => Faker::create()->randomFloat(2, 0.00, 40.00),
+                    'kelembaban' => Faker::create()->randomFloat(2, 40.00, 99.00),
+                    'pm25' => Faker::create()->randomFloat(2, 5.00, 150.00),
+                    'tekanan' => Faker::create()->randomFloat(2, 1000.00, 1025.00),
+                    'temperature' => Faker::create()->randomFloat(2, 22.00, 35.00),
+                    'total_hujan' => Faker::create()->randomFloat(2, 0.00, 500.00),
+                    'uv_index' => Faker::create()->randomFloat(2, 0.00, 11.00),
                     'recorded_at' => now()->subHours($i)->subMinutes(rand(0, 59)),
                     'created_at' => now(),
                     'updated_at' => now(),
